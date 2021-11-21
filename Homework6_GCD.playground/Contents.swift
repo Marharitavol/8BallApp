ecb46fbaa2bd37e3054d639570ef210370844a8f
Marharitavol <marharitavol@gmail.com>
2021-11-21 18:08:37 +0200

    Homework 6 completed

---
+++ Homework6_GCD.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+func deadlock() {
+    let queue = DispatchQueue(label: "com.gcd.serial")
+    queue.async {
+        print("A")
+        DispatchQueue.main.sync {
+            print("B")
+        }
+    }
+
+    queue.sync {
+        print("C")
+    }
+}
+
+func cancellationOfDispatchWorkItem() {
+    let queue = DispatchQueue.global(qos: .background)
+    var item: DispatchWorkItem?
+    
+    item = DispatchWorkItem {
+        while true {
+            if item?.isCancelled ?? true { break }
+            print("0")
+        }
+    }
+    
+    guard let _ = item else { return }
+    queue.async(execute: item!)
+    
+    queue.asyncAfter(deadline: .now() + 2) {
+        item?.cancel()
+    }
+}
+
+//deadlock()
+//cancellationOfDispatchWorkItem()
