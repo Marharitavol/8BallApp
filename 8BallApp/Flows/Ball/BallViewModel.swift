b89ccbdc3bcd0b34192b5a83168b1a10a1910d0a
Marharitavol <marharitavol@gmail.com>
2021-12-05 12:47:54 +0200

    Added shake animation.

--- 8BallApp/Flows/Ball/BallViewModel.swift
+++ 8BallApp/Flows/Ball/BallViewModel.swift
@@ -8,14 +8,21 @@
 import Foundation
 
 class BallViewModel {
+    var callback: ((Bool) -> Void)?
+    
     private let model: BallModel
+    private var animationTimeUp = false
+    private var didAnswerCome = false
     
     init(model: BallModel) {
         self.model = model
     }
     
     func shake(completion: @escaping (_ answer: String?) -> Void) {
+        startTime()
         model.fetchData { (answer) in
+            self.didAnswerCome = true
+            self.checkAnimationTime()
             let formattedAnswer = answer?.uppercased()
             completion(formattedAnswer)
         }
@@ -28,5 +35,20 @@ class BallViewModel {
     func saveHistory(_ answer: String) {
         let history = History(answer: answer, date: Date(), isLocal: false)
         model.saveHistory(history)
+    }
+    
+    private func startTime() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.animationTimeUp = true
+            self.checkAnimationTime()
+        }
+    }
+    
+    private func checkAnimationTime() {
+        let isReady = animationTimeUp && didAnswerCome
+        guard isReady else { return }
+        animationTimeUp.toggle()
+        didAnswerCome.toggle()
+        callback?(isReady)
     }
 }
